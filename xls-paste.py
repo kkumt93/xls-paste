44cb03d03e08a7ecb8766658187c9b345ef3b53a
kkumt93 <naniwa.k.m0903@gmail.com>
2017-12-04 03:52:13 +0900

    処理内容を変えずにiniとfile_listの引数削除

--- xls-paste.py
+++ xls-paste.py
@@ -1,40 +1,29 @@
 import xlwings as xw
 import sys
 
-#Input Ini File
 args = sys.argv
 
-#Read Ini File
-for line in open(args[1], "r"):
-    if line[0] == "#":
-        continue
-    ini = line.split()
-    
-    if(ini[0]=="xls_file"):
-        xls_file = ini[1]
-    if(ini[0]=="sheets"):
-        sheets = ini[1]
-    if(ini[0]=="out_file"):
-        out_file = ini[1]
+xls_file = args[1]
+out_file = args[2]
+ini_file = args[3]
 
 #Write  Excel
 wb = xw.Book(xls_file)
-for file_path in open("file_list.txt", "r"):
+for file_path in open(ini_file, "r"):
     if file_path[0] == "#":
         continue
     path = file_path.split()
 
-    #irow = int(path[2])
-    icol     = int(path[3])
-    offset_y = int(path[4])
-    offset_x = int(path[5])
+    col = sum(1 for line in open(path[0])) - 1
+    offset_y = int(path[2])
+    offset_x = int(path[3])
 
-    for y in range(icol):
+    for y in range(col):
         x = 0
         for data_path in open(path[0], "r"):
             data = data_path.split()
             wb.sheets[path[1]].range(x+offset_x+1, y+offset_y+1).value = data[y]
             x=x+1
-
+        
 #Save Excel
 wb.save(out_file)
