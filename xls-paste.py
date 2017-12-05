d404847d5d151b86c2ae1fccf9b19ea96598f1cd
kkumt93 <naniwa.k.m0903@gmail.com>
2017-12-05 20:52:42 +0900

    xy名前修正

--- xls-paste.py
+++ xls-paste.py
@@ -16,15 +16,15 @@ for line_data in open(file_list, "r"):
 
     col = sum(1 for line in open(split_line_data[0])) - 1
     sheets = split_line_data[1]
-    offset_y = int(split_line_data[2]) + 1
-    offset_x = int(split_line_data[3]) + 1
+    offset_x = int(split_line_data[2]) + 1
+    offset_y = int(split_line_data[3]) + 1
 
-    for y in range(col):
-        x = 0
+    for x in range(col):
+        y = 0
         for text_data in open(split_line_data[0], "r"):
             data = text_data.split()
-            wb.sheets[sheets].range(x+offset_x, y+offset_y).value = data[y]
-            x=x+1
+            wb.sheets[sheets].range(y+offset_y, x+offset_x).value = data[x]
+            y=y+1
         
 #Save Excel
 wb.save(out_file)
